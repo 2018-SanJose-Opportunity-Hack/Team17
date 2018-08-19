65e196c2f7bea414d56ffe89186acf3ba3133de6
Mohapatra <pankaj.mest@gmail.com>
2018-08-19 09:16:23 -0700

    after message pilot sucesss

--- Team17/make_call.py
+++ Team17/make_call.py
@@ -27,6 +27,27 @@ def call(twiml,to_num,from_num='+16504762892'):
     return call_obj
 
 
+def update_call_sid(sbo,match_id):
+    update = "update PCV_MATCHES set call_sid='{}' where match_sid={}".format(sbo.sid,match_id)
+    if update_query(update):
+        log.info('table updated with query {}'.format(update))
+    else:
+        log.error('table couldnot be updated')
+    while sbo.status != 'completed':
+        time.sleep(60)
+        sbo = client.calls(sbo.sid).fetch()
+        log.info('status of the call: {}'.format(sbo.status))
+    return sbo
+
+
+def update_call_duration(sbo,match_id):
+    update = "update PCV_MATCHES set call_duration='{}' where match_sid={}".format(sbo.duration,match_id)
+    if update_query(update):
+        log.info('table updated with query {}'.format(update))
+    else:
+        log.error('table couldnot be updated')
+
+
 def make_call(match_id):
     fetch = "select advisor_phone,sbo_phone,advisor_name,sbo_name from pcv_matches where match_sid={}".format(match_id)
     res = fetch_query(fetch)
@@ -38,24 +59,14 @@ def make_call(match_id):
         adv_ph = '+1'+adv_ph
     sbo = call(config.get('TWIML','sbo_twiml'),res[0][1])
     adv = call(config.get('TWIML','adv_twiml'),res[0][0])
-    update = "update PCV_MATCHES set call_sid='{}' where match_sid={}".format(sbo.sid,match_id)
-    if update_query(update):
-        log.info('table updated with query {}'.format(update))
-    else:
-        log.error('table couldnot be updated')
-    while sbo.status != 'completed':
-        time.sleep(60)
-        sbo = client.calls(sbo.sid).fetch()
-        log.info('status of the call: {}'.format(sbo.status))
-    update = "update PCV_MATCHES set call_duration='{}' where match_sid={}".format(sbo.duration,match_id)
-    if update_query(update):
-        log.info('table updated with query {}'.format(update))
-    else:
-        log.error('table couldnot be updated')
+    # update columns
+    sbo = update_call_sid(sbo,match_id)
+    update_call_duration(sbo,match_id)
+    # send message
     msg_body_sbo = "How did you like the call with {}".format(res[0][2])
     msg_body_adv = "How did you like the call with {}".format(res[0][3])
     sbo = send_sms(msg_body_sbo, sbo_ph)
-    sbo = send_sms(msg_body_sbo, adv_ph)
+    adv = send_sms(msg_body_adv, adv_ph)
 
 if __name__=="__main__":
     run()
